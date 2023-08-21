a26b8e1ee6624e576beac7a9039bc303e350f7e9
andyqwartz <141650489+andyqwartz@users.noreply.github.com>
2023-08-21 12:04:51 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -26,33 +26,3 @@ function onPlayerReady(event) {
     // This function can be used to do something when the player is ready, if needed
 }
 
-// Toggle mute function
-function toggleMute() {
-    toggleYouTubePlayersMute();
-    toggleMediaElementsMute();
-
-    // Update the mute status and button emoji
-    isMuted = !isMuted;
-    const muteButton = document.getElementById('muteButton');
-    muteButton.textContent = isMuted ? '🔇' : '🔊';
-}
-
-// Mute or unmute YouTube players
-function toggleYouTubePlayersMute() {
-    players.forEach(player => {
-        if (!isMuted) {
-            player.mute();
-        } else {
-            player.unMute();
-        }
-    });
-}
-
-// Mute or unmute all audio and video elements
-function toggleMediaElementsMute() {
-    const audios = document.querySelectorAll('audio');
-    const videos = document.querySelectorAll('video');
-    audios.forEach(audio => audio.muted = !isMuted);
-    videos.forEach(video => video.muted = !isMuted);
-}
-
